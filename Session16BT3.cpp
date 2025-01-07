88ae0a03a152055b645254d94e853ed639b65464
lethinh <68421373+lethinh26@users.noreply.github.com>
2025-01-07 21:07:42 +0700

    Update Session16BT3.cpp

--- Session16BT3.cpp
+++ Session16BT3.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void sum(int *a, int *b, int *res);
+void sum(int a, int b, int *res);
 
 int main() {
     int a = 5, b = 10, res;
-    sum(&a, &b, &res);
+    sum(a, b, &res);
     printf("a + b = %d", res);
     return 0;
 }
 
-void sum(int *a, int *b, int *res) {
-    *res = *a+*b;
+void sum(int a, int b, int *res) {
+    *res = a+b;
     return;
 }
